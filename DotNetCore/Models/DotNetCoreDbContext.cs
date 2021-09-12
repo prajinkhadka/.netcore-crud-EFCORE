7227b4f24ed0ec5dab89c9907ba6cd33e2c016ea
prajin <prawjeenkhadka@gmail.com>
2021-09-12 12:16:44 +0545

    CRUD operation using EFcore

---
+++ DotNetCore/Models/DotNetCoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCore.Models
+{
+    public class DotNetCoreDbContext : DbContext
+    {
+        public DotNetCoreDbContext(DbContextOptions<DotNetCoreDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
